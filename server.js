b21efb8ba5d933866f9b750b013cdd584d8938a1
Daniel Mendez <Daniel.Mendez@ey.com>
2018-11-01 23:44:55 -0600

    added eslint config file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
+app.get('/', (req, res) => res.send('hello world!'))
+
+app.listen(port, function() {
+  //  console.log('listening on 3000')
   })
